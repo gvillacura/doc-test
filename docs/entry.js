1108e9afbef0aba178a5452ec04a447ac1f9b195
gVillacura <geraldinevillacuraruz@gmail.com>
2022-11-15 18:34:45 -0300

    test

---
+++ docs/entry.js
@@ -0,0 +1,176 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import React from "react";
+
+      import ReactDOM from "react-dom";
+
+
+      import ReactWrapper from '../node_modules/better-docs/lib/react-wrapper.js';
+
+      window.React = React;
+
+      window.ReactDOM = ReactDOM;
+
+      window.ReactWrapper = ReactWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/components/results-tab/graphic-map-modal/BarGraphModal.tsx';
+reactComponents['BarGraphModal'] = Component0;
+
+import Component1 from '../src/components/results-tab/ColorsScale.tsx';
+reactComponents['ColorsScale'] = Component1;
+
+import Component2 from '../src/components/statesCountiesSelects/CountiesSelect.jsx';
+reactComponents['CountiesSelect'] = Component2;
+
+import Component3 from '../src/components/data-fit-tab/index.tsx';
+reactComponents['DataFitTab'] = Component3;
+
+import Component4 from '../src/components/results-tab/DoubleYAxis.tsx';
+reactComponents['DoubleYAxis'] = Component4;
+
+import Component5 from '../src/components/data-fit-tab/EndPointSource.tsx';
+reactComponents['EndPointSource'] = Component5;
+
+import Component6 from '../src/components/models-tab/ExportModels.tsx';
+reactComponents['ExportModels'] = Component6;
+
+import Component7 from '../src/components/data-fit-tab/FileSource.tsx';
+reactComponents['FileSource'] = Component7;
+
+import Component8 from '../src/components/data-fit-tab/FitParameterTable.tsx';
+reactComponents['FitParameterTable'] = Component8;
+
+import Component9 from '../src/components/data-fit-tab/FitParemetersTabs.tsx';
+reactComponents['FitParemetersTabs'] = Component9;
+
+import Component10 from '../src/components/map-tab/selectorMap/GeoSelectionsDetails.tsx';
+reactComponents['GeoSelectionsDetails'] = Component10;
+
+import Component11 from '../src/components/statesCountiesSelects/GeoSelectionsTab.tsx';
+reactComponents['GeoSelectionsTab'] = Component11;
+
+import Component12 from '../src/components/map-tab/selectorMap/GeoToastMessage.tsx';
+reactComponents['GeoToastMessage'] = Component12;
+
+import Component13 from '../src/components/results-tab/graphic-map-modal/GraphAndMapMetaModal.tsx';
+reactComponents['GraphAndMapMetaModal'] = Component13;
+
+import Component14 from '../src/components/results-tab/graphic-map-modal/GraphAndMapMonoModal.tsx';
+reactComponents['GraphAndMapMonoModal'] = Component14;
+
+import Component15 from '../src/components/results-tab/GraphicAndMapResults.tsx';
+reactComponents['GraphicAndMapResults'] = Component15;
+
+import Component16 from '../src/components/data-fit-tab/GraphicDataFit.tsx';
+reactComponents['GraphicDataFit'] = Component16;
+
+import Component17 from '../src/components/results-tab/graphic-map-modal/GraphModal.tsx';
+reactComponents['GraphModal'] = Component17;
+
+import Component18 from '../src/components/models-tab/ImportModels.tsx';
+reactComponents['ImportModels'] = Component18;
+
+import Component19 from '../src/components/new-model/InitialConditionsModel.tsx';
+reactComponents['InitialConditiosModels'] = Component19;
+
+import Component20 from '../src/components/map-tab/index.tsx';
+reactComponents['Map'] = Component20;
+
+import Component21 from '../src/components/results-tab/MapResults.tsx';
+reactComponents['MapResults'] = Component21;
+
+import Component22 from '../src/components/results-tab/metapopulation-map/MetaMapResults.tsx';
+reactComponents['MetaMapResults'] = Component22;
+
+import Component23 from '../src/components/data-fit-tab/MetapopulationDataFit.tsx';
+reactComponents['MetapopulationDataFit'] = Component23;
+
+import Component24 from '../src/components/results-tab/metapopulation-selection/MetapopulationSelectTable.tsx';
+reactComponents['MetapopulationSelectTable'] = Component24;
+
+import Component25 from '../src/components/new-model/ModelAccordion.tsx';
+reactComponents['ModelAccordion'] = Component25;
+
+import Component26 from '../src/components/new-model/ModelBuilder.tsx';
+reactComponents['ModelBuilder'] = Component26;
+
+import Component27 from '../src/components/new-model/ModelController.tsx';
+reactComponents['ModelController'] = Component27;
+
+import Component28 from '../src/components/new-model/ModelMainTab.tsx';
+reactComponents['ModelMainTab'] = Component28;
+
+import Component29 from '../src/components/data-fit-tab/MonopopulationDataFit.tsx';
+reactComponents['MonopopulationDataFit'] = Component29;
+
+import Component30 from '../src/components/new-model/index.tsx';
+reactComponents['NewModel'] = Component30;
+
+import Component31 from '../src/components/data-fit-tab/NodeSearchFilter.tsx';
+reactComponents['NodeSearchFilter'] = Component31;
+
+import Component32 from '../src/components/NumberInputEpi.tsx';
+reactComponents['NumberInputEpi'] = Component32;
+
+import Component33 from '../src/components/NumberInputVariableDependent.tsx';
+reactComponents['NumberInputVariableDependent'] = Component33;
+
+import Component34 from '../src/components/results-tab/PlayDataSlider.tsx';
+reactComponents['PlayDataSlider'] = Component34;
+
+import Component35 from '../src/components/results-tab/RealDataCheckBoxs.tsx';
+reactComponents['RealDataCheckBoxs'] = Component35;
+
+import Component36 from '../src/components/ResetAlerts.tsx';
+reactComponents['ResetAlerts'] = Component36;
+
+import Component37 from '../src/components/results-tab/index.tsx';
+reactComponents['Results'] = Component37;
+
+import Component38 from '../src/components/results-tab/ResultsDrawer.tsx';
+reactComponents['ResultsDrawer'] = Component38;
+
+import Component39 from '../src/components/results-tab/ResultsMapsSelection.tsx';
+reactComponents['ResultsMapsSelection'] = Component39;
+
+import Component40 from '../src/components/results-tab/ResultsSelection.tsx';
+reactComponents['ResultsSelection'] = Component40;
+
+import Component41 from '../src/components/summary-tab/RunButton.tsx';
+reactComponents['RunButton'] = Component41;
+
+import Component42 from '../src/components/data-fit-tab/SampleSource.tsx';
+reactComponents['SampleSource'] = Component42;
+
+import Component43 from '../src/components/new-model/SelectDate.tsx';
+reactComponents['SelectDate'] = Component43;
+
+import Component44 from '../src/components/side-selector-feature/SelectedFeaturesPanel.tsx';
+reactComponents['SelectedFeaturesPanel'] = Component44;
+
+import Component45 from '../src/components/map-tab/selectorMap/SelectorMap.jsx';
+reactComponents['SelectorMap'] = Component45;
+
+import Component46 from '../src/components/map-tab/selectorMap/SelectorMapAccordion.tsx';
+reactComponents['SelectorMapAccordion'] = Component46;
+
+import Component47 from '../src/components/statesCountiesSelects/StatesSelect.jsx';
+reactComponents['StatesSelect'] = Component47;
+
+import Component48 from '../src/components/side-selector-feature/StatesSelectedCheckbox.tsx';
+reactComponents['StatesSelectedCheckbox'] = Component48;
+
+import Component49 from '../src/components/summary-tab/SummaryTab.tsx';
+reactComponents['SummaryTab'] = Component49;
+
+import Component50 from '../src/components/summary-tab/TableSimulations.tsx';
+reactComponents['TableSimulations'] = Component50;
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EPIc Suite'
+copyright = '2022, Dlab'
+author = 'Dlab'
 
 release = '0.1'
 version = '0.1.0'
